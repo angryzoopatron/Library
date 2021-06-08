5e8e005b64f11536392a897ee4b48326216b91b7
Mike <iamahab@gmail.com>
2021-06-07 21:08:45 -0400

    still working on this piece of crap because I am an idiot

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
 const addBtn = document.querySelector('#addBtn');
-const newBookBtn = document.querySelector('#newBtn');
+const newBtn = document.querySelector('#newBtn');
 const popUpForm = document.getElementById('popUp');
-const closePopUp = document.getElementsByTagName('span')[0];
+const closePopup = document.getElementsByTagName('span')[0];
 
-addBtn.addEventListener('click', addBookToLibrary);
-newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+
+function testFunction(){
+    console.log('fuck');
+}
+
+addBtn.addEventListener('click', testFunction);
+newBtn.addEventListener('click', () => popUpForm.style.display = 'block');
 closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
 
+//  https://github.com/CatQueenCodes/Project-Library
 
-//Book Constructor
-class Book {
-    constructor(title, author, pages, read) {
-        this.title = form.title.value; 
-        this.author = form.author.value; 
-        this.pages = form.pages.value + 'pg'; 
-        this.read = form.read.checked; 
-    }
-}
-
-//creates book from Book Constructor, adds to library
-let myLibrary = [];
-let newBook;
-
-function addBookToLibrary(event) {
-    event.preventDefault();
-    popUpForm.style.display = 'none';
-
-    newBook = new Book(title, author, pages,read); 
-    myLibrary.push(newBook); 
-    //setData();  //saves updated array in local storage
-    // render(); 
-    form.reset();
-}
+// referencing that for notes because I am an idiot
