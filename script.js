5ed593162bd27f16bd9901b786295e20b90f7358
angryzoopatron <81863211+angryzoopatron@users.noreply.github.com>
2021-06-08 08:58:33 -0400

    Update script.js
    updated on break 6/8

--- script.js
+++ script.js
@@ -4,13 +4,46 @@ const popUpForm = document.getElementById('popUp');
 const closePopup = document.getElementsByTagName('span')[0];
 
 
-function testFunction(){
-    console.log('fuck');
+function testFunction() {
+    console.log('poo');
 }
+
+class Book {
+    constructor(title) {
+        this.title = form.title.value;
+    }
+}
+
+let library = [];
+let book;
+
+// create function to push books to library array
+function addBookToLibrary(e) {
+    e.preventDefault;
+    popUpForm.style.display = 'none';
+    book = new Book(title);
+    library.push(book);
+
+    form.reset();
+}
+
+function displayBooks() {
+    const display = document.getElementById('lib-container');
+    const books = document.querySelector('.book')
+}
+
+function createBook() {
+
+}
+
 
 addBtn.addEventListener('click', testFunction);
 newBtn.addEventListener('click', () => popUpForm.style.display = 'block');
 closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
+
+
+
+
 
 //  https://github.com/CatQueenCodes/Project-Library
 
