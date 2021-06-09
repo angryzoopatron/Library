8245cda07d2f617efc0b06f145c350864635c418
angryzoopatron <81863211+angryzoopatron@users.noreply.github.com>
2021-06-09 13:58:44 -0400

    added dispatch
    added dispatch events to clean it up

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // button variables for event listeners
+const library = document.querySelector('#lib-container');
 const addBtn = document.querySelector('#addBtn');
 const newBookBtn = document.querySelector('#newBtn');
 const popUpForm = document.getElementById('popUp');
@@ -7,10 +8,10 @@ const closePopUp = document.getElementsByTagName('span')[0];
 //Book Constructor
 class Book {
     constructor(title, author, pages, read) {
-        this.title = title; 
-        this.author = author; 
-        this.pages = pages; 
-        this.read = read; 
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 }
 
@@ -32,9 +33,10 @@ function addBookToLibrary() {
     }
 
 
-    myLibrary.push(newBook); 
-    setData();  //saves updated array in local storage
-    displayBooks(); // will change to custom dispatch
+    myLibrary.push(newBook);
+    //setData();
+    // displayBooks();
+    library.dispatchEvent(new CustomEvent('libUpdated'))
     form.reset();
 }
 
@@ -43,67 +45,69 @@ function displayBooks() {
     const display = document.getElementById('lib-container');
     const books = document.querySelectorAll('.book');
     books.forEach(book => display.removeChild(book));
-    
-    for (let i of myLibrary){
+
+    for (let i of myLibrary) {
         createBook(i);
     }
 }
 
 //creates book DOM elements, to use in displayBooks();
 function createBook(item) {
-    const library = document.querySelector('#lib-container');
     const bookDiv = document.createElement('div');
     const titleDiv = document.createElement('div');
     const authDiv = document.createElement('div');
     const pageDiv = document.createElement('div');
     const removeBtn = document.createElement('button');
     const readBtn = document.createElement('button');
-    
-    
+
+
     bookDiv.classList.add('book');
     titleDiv.classList.add('title');
     authDiv.classList.add('author');
     pageDiv.classList.add('pages');
-    readBtn.classList.add('readBtn')    
+    readBtn.classList.add('readBtn')
 
     bookDiv.setAttribute('id', myLibrary.indexOf(item));
     removeBtn.setAttribute('id', 'removeBtn');
-    
+
     titleDiv.textContent = item.title;
     authDiv.textContent = item.author;
     pageDiv.textContent = item.pages;
-    removeBtn.textContent = 'Remove'; 
-    
+    removeBtn.textContent = 'Remove';
+
     bookDiv.appendChild(titleDiv);
     bookDiv.appendChild(authDiv);
     bookDiv.appendChild(pageDiv);
     bookDiv.appendChild(readBtn);
 
-    if(item.read === false) {
+    if (item.read === false) {
         readBtn.textContent = 'Not Read';
         readBtn.style.backgroundColor = 'red';
-    }else {
+    } else {
         readBtn.textContent = 'Read';
         readBtn.style.backgroundColor = 'green'
     }
-    
+
     bookDiv.appendChild(removeBtn);
-    
+
     library.appendChild(bookDiv);
 
     // remove book from DOM via splice from index
     removeBtn.addEventListener('click', () => {
-        myLibrary.splice(myLibrary.indexOf(item),1);
-        setData()
-        displayBooks();
+        myLibrary.splice(myLibrary.indexOf(item), 1);
+        //setData()
+        //displayBooks();
+        library.dispatchEvent(new CustomEvent('libUpdated'))
+
     });
-    
+
     // toggle read status onClick
-    readBtn.addEventListener('click', () => { 
-        item.read = !item.read; 
-        setData(); 
-        displayBooks();
-    }); 
+    readBtn.addEventListener('click', () => {
+        item.read = !item.read;
+        // setData();
+        // displayBooks();
+        library.dispatchEvent(new CustomEvent('libUpdated'))
+    });
 };
 
 // local storage needs objects converted to properly store data, json.stringify does so
@@ -113,13 +117,16 @@ function setData() {
 
 // restores state from local storage
 function restore() {
-    if(!localStorage.myLibrary) {
-        displayBooks();
-    }else {
+    if (!localStorage.myLibrary) {
+        // displayBooks();
+        library.dispatchEvent(new CustomEvent('libUpdated'))
+
+    } else {
         let objects = localStorage.getItem('myLibrary') // gets information from local storage to use in below loop to create DOM/display
         objects = JSON.parse(objects);
         myLibrary = objects;
-        displayBooks();
+        //displayBooks();
+        library.dispatchEvent(new CustomEvent('libUpdated'))
     }
 }
 
@@ -130,6 +137,8 @@ closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
 
 // will add dispatch below this comment to loosen coupling
 
+library.addEventListener('libUpdated', displayBooks);
+library.addEventListener('libUpdated', setData);
 
 // restore function
 restore();
